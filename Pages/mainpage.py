43e25e9187eb580d9cc627faf8325c8c38764c16
knoppix123 <knoppix123@gmail.com>
2023-02-17 00:06:39 -0500

    added test top menu buttons

--- Pages/mainpage.py
+++ Pages/mainpage.py
@@ -12,17 +12,36 @@ class Mainpage:
         self.driver = driver
     # =======main buttons=====
     mainlogo = (By.XPATH, "//img[@id='logo']")
-    main_button = ( By.CSS_SELECTOR, "[class='test menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-98']")
-
-
-
-
-
+    button_main = ( By.CSS_SELECTOR, "[class='test menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-98']")
+    button_store_locations = (By.CSS_SELECTOR, "[id='menu-item-135']")
+    button_catering = (By.CSS_SELECTOR, "[id='menu-item-871']")
+    button_loyalty_program = (By.CSS_SELECTOR, "[id='menu-item-3908']")
+    button_gift_cards = (By.CSS_SELECTOR, "[id='menu-item-3888']")
+    button_about = (By.CSS_SELECTOR, "[id='menu-item-136']")
+    button_franchise = (By.CSS_SELECTOR, "[id='menu-item-1646']")
+    button_order_now = (By.CSS_SELECTOR, "[id='menu-item-10638']")
 
     def getmainlogo(self):
         return self.driver.find_element(*Mainpage.mainlogo)
-    def getmainbutton(self):
-        return self.driver.find_element(*Mainpage.main_button)
+    def getbutton_menu(self):
+        return self.driver.find_element(*Mainpage.button_main)
+    def getbutton_store_locations(self):
+        return self.driver.find_element(*Mainpage.button_store_locations)
+    def getbutton_catering(self):
+        return self.driver.find_element(*Mainpage.button_catering)
+    def getbutton_loyalty_program(self):
+        return self.driver.find_element(*Mainpage.button_loyalty_program)
+    def getbutton_gift_cards(self):
+        return self.driver.find_element(*Mainpage.button_gift_cards)
+    def getbutton_about(self):
+        return self.driver.find_element(*Mainpage.button_about)
+    def getbutton_franchise(self):
+        return self.driver.find_element(*Mainpage.button_franchise)
+    def getbutton_order_now(self):
+        return self.driver.find_element(*Mainpage.button_order_now)
+
+
+
 
 
 
--- Tests/test_mainpage.py
+++ Tests/test_mainpage.py
@@ -17,14 +17,61 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Testmenuitems(BaseClass):
 
-    def testmenu(self):
+    def test_top_menu_buttons(self):
         HP = Mainpage(self.driver)
         HP.getmainlogo()
         time.sleep(2)
 
-        HP.getmainbutton().click()
+        HP.getbutton_menu().click()
         title = self.driver.title
         print(title)
         assert title == "Our Menu | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_store_locations().click()
+        title = self.driver.title
+        assert title == "Store Locations | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_catering().click()
+        title = self.driver.title
+        assert title == "Catering - Share Fresh Foods With Style! | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_loyalty_program().click()
+        title = self.driver.title
+        assert title == "Loyalty Program - Eat Healthy and Earn Points | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_gift_cards().click()
+        title = self.driver.title
+        assert title == "Gift Cards | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_about().click()
+        title = self.driver.title
+        assert title == "About Vitality Bowls | Açaí Bowls | Vitality Bowls"
+        HP.getmainlogo().click()
+
+        HP.getbutton_franchise().click()
+        title = self.driver.title
+        assert title == "WHO WE ARE | Vitality Bowls"
+        self.driver.back()
+
+        HP.getbutton_order_now().click()
+        title = self.driver.title
+        assert title == "Vitality Bowls"
+        self.driver.back()
+        #
+
+
+
+
+
+
+
+
+
+
 
 
